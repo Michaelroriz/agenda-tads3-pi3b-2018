809541f813acf5b17e19c0563e9307e8880f4ad2
michael.aroriz <michael.aroriz@CAS511899W1020.senacsp.edu.br>
2018-08-23 22:10:53 -0300

    alterações da classe

--- agenda/src/main/java/br/senac/tads/pi3b/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads/pi3b/agenda/Agenda.java
@@ -5,7 +5,11 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,6 +18,8 @@ import java.util.logging.Logger;
  */
 public class Agenda {
 
+    private static Scanner entrada = new Scanner(System.in);
+    
     private Connection obterConexao() throws ClassNotFoundException, SQLException {
         Connection conn = null;
         //Passo 1: Registrar driver JDBC
@@ -47,8 +53,60 @@ public class Agenda {
         }
     }
 
+    public void inserir() {
+
+        System.out.print("Digite um nome: ");
+        String name = entrada.nextLine();
+        System.out.print("Digite a data de nascimento no formato dd/mm/aaaa: ");
+        String dateN = entrada.nextLine();
+
+        String querySql = "INSERT INTO PESSOA (NOME, DTNASCIMENTO) VALUES (?,?)";
+
+        try {
+            Connection conn = obterConexao();
+            PreparedStatement stmt = conn.prepareStatement(querySql);
+            stmt.setString(1, name);
+            DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+            Date dataNasc = null;
+            try {
+                dataNasc = format.parse(dateN);
+            } catch (ParseException ex) {
+                Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("Data de nascimento invalida");
+            }
+            stmt.setDate(2, new java.sql.Date(dataNasc.getTime()));
+            stmt.executeUpdate();
+            System.out.println("Pessoa cadastrada com sucesso");
+
+        } catch (SQLException e) {
+            System.out.println("Nao foi possivel executar");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Nao foi possivel executar");
+        }
+    }
+
+    public void deletar() {
+        System.out.print("Qual ID da pessoa que deseja deletar: ");
+        int id = entrada.nextInt();
+
+        String querySql = "DELETE FROM PESSOA WHERE ID =" + id;
+        
+        try {
+            Connection conn = obterConexao();
+            PreparedStatement stmt = conn.prepareStatement(querySql);            
+            stmt.execute(querySql, id);
+        }catch (ClassNotFoundException ex) {
+            Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     public static void main(String[] args) {
         Agenda agenda = new Agenda();
         agenda.executar();
+        agenda.deletar();
+        agenda.executar();
+
     }
 }
